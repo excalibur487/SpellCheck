61ece90ade01d89629a0cdfe88fc9b5dcc0861e6
sshinde4 <siddarth2003s@gmail.com>
2023-08-22 13:25:52 -0400

    Adding benchmarking

--- src/table.c
+++ src/table.c
@@ -5,11 +5,19 @@
 #include <string.h>
 #include <strings.h>
 #include <math.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 #include "/Users/siddarthshinde/dev/projects/SpellCheck/inc/list.h"
 #include "/Users/siddarthshinde/dev/projects/SpellCheck/inc/table.h"
 
 #define DICTIONARY "/Users/siddarthshinde/dev/projects/SpellCheck/test/test-files/large.txt"
+
+#undef calculate
+#undef getrusage
+
+// Prototype
+double calculate(const struct rusage *b, const struct rusage *a);
 
 /**
  * @brief Hash Table
@@ -158,10 +166,19 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Structures for timing data
+    struct rusage before, after;
+
+    // Benchmarks
+    double time_load = 0.0, time_check = 0.0, time_size = 0.0, time_unload = 0.0;
+
     // Determine dictionary to use
     char *dictionary = (argc == 3) ? argv[1] : DICTIONARY;
 
+    // Load dictionary
+    getrusage(RUSAGE_SELF, &before);
     bool loaded = populate(dictionary);
+    getrusage(RUSAGE_SELF, &after);
 
     // Exit if dictionary not loaded
     if (!loaded)
@@ -169,6 +186,9 @@ int main(int argc, char *argv[])
         printf("Could not load %s.\n", dictionary);
         return 1;
     }
+
+    // Calculate time to load dictionary
+    time_load = calculate(&before, &after);
 
     char *text = (argc == 3) ? argv[2] : argv[1];
     FILE *file = fopen(text, "r");
@@ -228,7 +248,13 @@ int main(int argc, char *argv[])
             // Update counter
             words++;
 
+            // Check word's spelling
+            getrusage(RUSAGE_SELF, &before);
             bool misspelled = !check(word);
+            getrusage(RUSAGE_SELF, &after);
+
+            // Update benchmark
+            time_check += calculate(&before, &after);
 
             // Print word if misspelled
             if (misspelled)
@@ -254,12 +280,22 @@ int main(int argc, char *argv[])
     // Close text
     fclose(file);
 
+    // Determine dictionary's size
+    getrusage(RUSAGE_SELF, &before);
     unsigned int n = size();
+    getrusage(RUSAGE_SELF, &after);
+
+    // Calculate time to determine dictionary's size
+    time_size = calculate(&before, &after);
+
+    // Unload dictionary
+    getrusage(RUSAGE_SELF, &before);
+    bool unloaded = free_dict();
+    getrusage(RUSAGE_SELF, &after);
+
     printf("\nWORDS MISSPELLED:     %d\n", misspellings);
     printf("WORDS IN DICTIONARY:  %d\n", n);
     printf("WORDS IN TEXT:        %d\n", words);
-
-    bool unloaded = free_dict();
 
     // Abort if dictionary not unloaded
     if (!unloaded)
@@ -267,6 +303,31 @@ int main(int argc, char *argv[])
         printf("Could not unload %s.\n", dictionary);
         return 1;
     }
+    // Calculate time to unload dictionary
+    time_unload = calculate(&before, &after);
 
+    printf("TIME IN load:         %.2f\n", time_load);
+    printf("TIME IN check:        %.2f\n", time_check);
+    printf("TIME IN size:         %.2f\n", time_size);
+    printf("TIME IN unload:       %.2f\n", time_unload);
+    printf("TIME IN TOTAL:        %.2f\n\n",
+           time_load + time_check + time_size + time_unload);
     return 0;
+}
+
+// Returns number of seconds between b and a
+double calculate(const struct rusage *b, const struct rusage *a)
+{
+    if (b == NULL || a == NULL)
+    {
+        return 0.0;
+    }
+    else
+    {
+        return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
+                  (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
+                 ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
+                  (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec))) /
+                1000000.0);
+    }
 }
